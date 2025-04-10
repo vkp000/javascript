c7c56b74883361268ffe5327a8cb622495ff1991
Vivek prajapat <112654448+vkp000@users.noreply.github.com>
2025-04-10 09:12:59 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "vkp@gmail.com"
+var accountPassword = "1234"
+accountCity = "Bhilwara"
+let accountState ;
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "bengluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
