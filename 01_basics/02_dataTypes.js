be2d888182e1728112012f226c5368a0472fa8e0
Vivek prajapat <112654448+vkp000@users.noreply.github.com>
2025-04-10 09:11:53 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+console.log(3+3);
+console.log("vivek")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null
+// undefined => 
+// symbol => jab bhi hume baat karni ho unique ke liye
+// object
+
+console.log(typeof "vivek");
+console.log(typeof null);
+console.log(typeof undefined);
