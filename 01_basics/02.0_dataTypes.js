7e4c0c014ce46bb3684ad480002313c73f246fef
vivek <vivekprajapatvkp0@gmail.com>
2025-04-11 18:37:59 +0530

    updated

--- 01_basics/02.0_dataTypes.js
+++ 01_basics/02.0_dataTypes.js
@@ -1,6 +1,6 @@
 "use strict"; // treat all JS code as newer version
 
-// alert(3+3) // we are using node.js not browser
+// alert(3+3) // we are using node.js not browser // 
 
 console.log(3+3);
 console.log("vivek")
